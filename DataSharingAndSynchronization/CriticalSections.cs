336b779f9658508180ad6dc647286711aeb6f97e
Андрей Брижак <andrewbrizhak@gmail.com>
2020-07-30 19:09:53 +0800

    add project  Data Sharing And Synchronization

---
+++ DataSharingAndSynchronization/CriticalSections.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace DataSharingAndSynchronization
+{
+    class CriticalSections
+    {
+        class BankAccount
+        {
+            public object padlock = new object();
+            public int Balance { get; private set; }
+
+            public void Deposit(int amount)
+            {
+
+                lock (padlock)
+                {
+                    // += is really two operations
+                    // op1 is temp <- get_Balance() + amount
+                    // op2 is set_Balance(temp)
+                    // something can happen _between_ op1 and op2
+
+                    Balance += amount;
+                }
+            }
+        }
+
+        public void Withdraw(int amount)
+        {
+            lock (padlock)
+            {
+                Balance -= amount;
+            }
+        }
+    }
+}
