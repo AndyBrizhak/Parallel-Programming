f853af667efe9d97ac6f82bfefa5970a3f2db3c3
Андрей Брижак <andrewbrizhak@gmail.com>
2020-08-05 21:31:14 +0800

    +

--- DataSharingAndSynchronization/CriticalSections.cs
+++ DataSharingAndSynchronization/CriticalSections.cs
@@ -9,24 +9,30 @@ namespace DataSharingAndSynchronization
 {
     class CriticalSections
     {
-        class BankAccount
+        private class BankAccount
         {
+            private readonly BankAccount _bankAccount = new BankAccount();
             public object padlock = new object();
             public int Balance { get; private set; }
 
             public void Deposit(int amount)
             {
 
-                lock (padlock)
-                {
-                    // += is really two operations
-                    // op1 is temp <- get_Balance() + amount
-                    // op2 is set_Balance(temp)
-                    // something can happen _between_ op1 and op2
 
-                    Balance += amount;
-                }
+                //lock (padlock)
+                //{
+                //    // += is really two operations
+                //    // op1 is temp <- get_Balance() + amount
+                //    // op2 is set_Balance(temp)
+                //    // something can happen _between_ op1 and op2
+
+                //    Balance += amount;
+                //}
             }
+        }
+
+        class BankAccount
+        {
         }
 
         public void Withdraw(int amount)
